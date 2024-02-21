6825696584783991bcbdee5f372f403d3bbedfaa
AlonsoMoreno <69394348+ph0nsy@users.noreply.github.com>
2024-02-21 19:17:00 +0100

    Http call 03 - Testing

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ date = "2024-02-21" #todayIs
 planets = []
 for i, nasaid in enumerate([1, 2, 3, 4, 5, 6, 7, 8]):  # The 1st, 2nd, 3rd (etc) planet in solar system
     obj = Horizons(id=nasaid, location="@sun", epochs=Time(date).jd).vectors()
+    console.log(obj)
     planets.append([np.double(obj[xi]) for xi in ['x', 'y', 'z']])
 
 planets = [coord[:2] for coord in planets]
 planets = np.array(planets)
 planets = planets/planets.max() # normalized planets
-print(planets)
 console.log(planets)
 
 # vvvvv Identify sector vvvvv (?)
