9917ac2dd8d2c4638331fadd9bb46447d9e8f0c9
AlonsoMoreno <69394348+ph0nsy@users.noreply.github.com>
2024-02-22 07:20:48 +0100

    Http call 05.7 - Testing

--- main.py
+++ main.py
@@ -4,23 +4,16 @@ import numpy as np
 from astropy.time import Time
 #!pip install astroquery
 from astroquery.jplhorizons import Horizons
-#!pip install datetime
 from datetime import date
-#!pip install pyscript
-from js import console
-from pyscript import document
-
-date = date.today() 
-console.log(date)
 
 planets = []
 for i, nasaid in enumerate([1, 2, 3, 4, 5, 6, 7, 8]):  # The 1st, 2nd, 3rd (etc) planet in solar system
-    obj = await Horizons(id=nasaid, location="@sun", epochs=Time(date).jd)
+    obj = await Horizons(id=nasaid, location="@sun", epochs=Time(date.today()).jd)
     planets.append([np.double(obj.vectors()[xi]) for xi in ['x', 'y']])
 
 #planets = [coord[:2] for coord in planets]
 planets = np.array(planets)
 planets = planets/planets.max() # normalized planets
-console.log(planets)
+print(planets)
 
 # vvvvv Identify sector vvvvv (?)
