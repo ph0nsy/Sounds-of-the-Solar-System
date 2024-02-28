5e32811685ecccbbe963ea8f5e1a2fc9223f7c4b
AlonsoMoreno <69394348+ph0nsy@users.noreply.github.com>
2024-02-28 07:03:09 +0100

    Http call 05.99 - Testing

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
-#!pip install numpy
 import numpy as np
-#!pip install astropy
+
 from astropy.time import Time
-#!pip install astroquery
+
 from astroquery.jplhorizons import Horizons
 from datetime import date
 
-print(date.today())
+print(date.today().strftime('%Y-%m-%d'))
+planets_obj = []
 planets = []
 for i, nasaid in enumerate([1, 2, 3, 4, 5, 6, 7, 8]):  # The 1st, 2nd, 3rd (etc) planet in solar system
-    obj = await Horizons(id=nasaid, location="@sun", epochs=Time(date.today()).jd)
-    planets.append([np.double(obj.vectors()[xi]) for xi in ['x', 'y']])
-
-#planets = [coord[:2] for coord in planets]
-planets = np.array(planets)
-planets = planets/planets.max() # normalized planets
+    obj = Horizons(id=nasaid, location="@sun", epochs=Time(date.today().strftime('%Y-%m-%d')).jd).vectors()
+    planets_obj.append(obj)
+    planets.append([np.double(obj[xi]) for xi in ['x', 'y']])
 print(planets)
+
+#print(planets_obj)
+#planets = [coord[:2] for coord in planets]
+#planets = np.array(planets)
+#planets = planets/planets.max() # normalized planets
+#print(planets)
 
 # vvvvv Identify sector vvvvv (?)
